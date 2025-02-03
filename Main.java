90433b5e32b976316e29caa8ac5f7e509f157318
rayl-tanmay2 <tanmay.joshi.btech2023@sitpune.edu.in>
2025-02-03 20:39:35 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,8 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayInput arrayInput = new ArrayInput();
+        ArrayFunctions functions = new ArrayFunctions();
 
