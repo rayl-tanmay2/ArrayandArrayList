6478d47ad20d2a1abd9affffd2bef85caa4dd0c8
rayl-tanmay2 <tanmay.joshi.btech2023@sitpune.edu.in>
2025-02-03 20:34:32 +0530

    Update ArrayFunctions.java

--- ArrayFunctions.java
+++ ArrayFunctions.java
@@ -5,3 +5,19 @@ class ArrayFunctions{
 		System.out.println("Array is: "+ array);
 	}
 
+void separateOddEven(int[] array){
+		ArrayList<Integer> even = new ArrayList<Integer>();
+		ArrayList<Integer> odd = new ArrayList<Integer>();
+		for(int i=0; i<array.length; i++){
+			if (array[i] % 2 == 0){
+				even.add(array[i]);
+			}
+			else{
+				odd.add(array[i]);
+			}
+		}
+		System.out.print("Even ");
+		display(even);
+		System.out.print("Odd ");
+		display(odd);
+	}
