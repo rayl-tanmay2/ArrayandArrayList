58a5e8fba57475c813f573097aed95ee1433779f
rayl-tanmay2 <tanmay.joshi.btech2023@sitpune.edu.in>
2025-02-03 20:42:12 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,3 +10,18 @@ class Main {
             System.out.println("\nSelect an operation:");
             System.out.println("1. Separate Even and Odd numbers\n2. Find index of closest neighboring numbers\n3. Convert Array to ArrayList\n4. Convert ArrayList to Array\n0. Exit");
             int choice = scanner.nextInt();
+            switch (choice) {
+                case 1 -> {
+                    int[] array = arrayInput.getUserArray();
+                    functions.separateOddEven(array);
+                }
+                case 2 -> {
+                    int[] array = arrayInput.getUserArray();
+                    int index = functions.findSmallestDistanceIndex(array);
+                    System.out.println("Index of the first number in the closest pair: " + index);
+                }
+                case 3 -> {
+                    int[] array = arrayInput.getUserArray();
+                    ArrayList<Integer> arrayList = functions.arrayToArrayList(array);
+                    System.out.println("Converted ArrayList: " + arrayList);
+                }
